da00e39c72f843dfff5a9fd1fd32e7c9db5240c7
mike <michal.kocztorz@msales.com>
2017-02-27 19:52:52 +0100

    adding mysql

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\DBAL\Connection;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,11 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        /** @var Connection $connection */
+        $connection = $this->getDoctrine()->getConnection();
+        $connection->connect();
+        $connection->executeQuery('SELECT * FROM aaa');
+        $connection->close();
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
